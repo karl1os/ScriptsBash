e79d39c73f1d3b2cb5d9c362dd95518f29dfb944
karl1os <44100512+karl1os@users.noreply.github.com>
2021-10-26 19:00:25 +0200

    Update scriptcarving.sh

--- scriptcarving.sh
+++ scriptcarving.sh
@@ -1,10 +1,10 @@
 # =====================================
 # title             : scriptcarving.sh
 # description       : Script que de forma automatitzada recupera un 
-#					  archiu .png de una imatge raw.
+#					            archiu .png de una imatge raw.
 # author            : Carlos Hernandez Navarro
 # date              : 25/10/2021
-# version			: 1.0
+# version           : 1.2
 # =====================================
 #!/bin/bash
 # Primer busca la linea on comenza el fitcher amb el numero magic i 
@@ -17,13 +17,21 @@ sed -i 's/:/ /g' tmpfirstline.txt
 # primera paraula 
 principio=$(awk '{print $1}' tmpfirstline.txt | tr [[:lower:]] [[:upper:]])
 # pasa a decimal el numero de linea
+#otraforma: principiod=$((16#$principio))
 principiod=$(echo "ibase=16;$principio" | bc)
 # repeteix el proces amb el final de l'archiu
 xxd imagen | sed 's/ //g' | grep 49454e44ae426082 > tmplastline.txt
 sed -i 's/:/ /g' tmplastline.txt
 final=$(awk '{print $1}' tmplastline.txt | tr [[:lower:]] [[:upper:]])
+# ahora extraemos el numero de bits a sumar al final del archivo
+sed 's/49454e44ae426082/ /g' tmplastline.txt
+bits=$(awk '{print $2}' tmplastline.txt | wc -c)
+bits=$(($bits-1))
+bits2=$(echo "49454e44ae426082" | wc -c)
+bits2=$(($bits2-1))
+bits=$((($bits+$bits2)/2))
 # Suma els bites de inforamcio a la linea
-finald=$(echo "ibase=16;$final+B" | bc)
+finald=$(echo "ibase=16;$final+$bits" | bc)
 # Restem per saber el tamany de l'archiu
 size=$(echo "$(($finald - $principiod))")
 #volcem l'a informacio a un archiu amb la seva extensio.
