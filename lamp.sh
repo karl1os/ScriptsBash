2cec151116ce040afe5cb17e46cbbba4d4ebf738
karl1os <44100512+karl1os@users.noreply.github.com>
2021-10-29 14:10:34 +0200

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -18,16 +18,17 @@ ufw allow in "Apache Full"
 mkdir -p /var/www/$resp
 chown -R $USER:$USER /var/www/$resp
 chmod -R 755 /var/www/$resp
-touch /etc/apache2/sites-available/$resp.conf
-echo "<VirtualHost *:80>" > etc/apache2/sites-available/$resp.conf
-echo "      ServerAdmin admin@$resp.com" >> etc/apache2/sites-available/$resp.conf
-echo "      ServerName $resp.com" >> etc/apache2/sites-available/$resp.conf
-echo "      ServerAlias www.$resp.com" >> etc/apache2/sites-available/$resp.conf
-echo "      DocumentRoot /var/www/$resp" >> etc/apache2/sites-available/$resp.conf
-echo >> etc/apache2/sites-available/$resp.conf
-echo "      ErrorLog ${APACHE_LOG_DIR}/error.log" >> etc/apache2/sites-available/$resp.conf
-echo "      CustomLog ${APACHE_LOG_DIR}/access.log combined" >> etc/apache2/sites-available/$resp.conf
-echo "</VirtualHost>" >> etc/apache2/sites-available/$resp.conf
+cat > /etc/apache2/sites-available/$resp.conf << EOF
+<VirtualHost *:80>
+      ServerAdmin admin@$resp.com
+      ServerName $resp.com
+      ServerAlias www.$resp.com
+      DocumentRoot /var/www/$resp
+
+      ErrorLog ${APACHE_LOG_DIR}/error.log
+      CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
 a2ensite $resp
 systemctl restart apache2
 apt install php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
